4d7edadd855b229fad725858b3c6f4637b36a6ac
rabindratharu <rabindra.tharu.np@gmail.com>
2026-04-30 08:30:28 +0545

    initial setup

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <div>Next JS &amp; WordPress course.</div>;
+
+export default function Home(props) {
+  console.log(props);
+  return <div dangerouslySetInnerHTML={{ __html: props.title }} />;
+}
+
+
+export async function getStaticProps() {
+  return {
+    props: {
+      title: "Next JS &amp; WordPress course.",
+      description: "Learn how to build a Next JS &amp; WordPress website.",
+    },
+  };
 }
