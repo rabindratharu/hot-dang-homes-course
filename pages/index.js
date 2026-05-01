0cac05706472161c875867bfc93022e8f7f4dd89
rabindratharu <rabindra.tharu.np@gmail.com>
2026-05-01 07:21:20 +0545

    added apolloClient

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-
+import { gql } from "@apollo/client";
+import client from "../lib/apolloClient";   // or use initializeApollo()
 export default function Home(props) {
   console.log(props);
   return <div dangerouslySetInnerHTML={{ __html: props.title }} />;
@@ -6,8 +7,20 @@ export default function Home(props) {
 
 
 export async function getStaticProps() {
+  const { data } = await client.query({
+    query: gql`
+      query GetPages {
+        pages {
+          nodes {
+            title
+          }
+        }
+      }
+    `,
+  });
   return {
     props: {
+      data,
       title: "Next JS &amp; WordPress course.",
       description: "Learn how to build a Next JS &amp; WordPress website.",
     },
