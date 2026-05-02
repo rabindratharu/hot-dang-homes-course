63b35526a5a3fe0f284e9e8d61908816bc3675d5
rabindratharu <rabindra.tharu.np@gmail.com>
2026-05-02 20:52:59 +0545

    organize queries and fetch data

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,11 @@
-import { gql } from "@apollo/client";
-import client from "../lib/apolloClient.js";   // or use initializeApollo()
+
+import { getHomePageData } from "../lib/useWordPressData.js";
 import { BlockRenderer } from "../components/BlockRenderer/BlockRenderer.js";
 import { cleanTransformBlocks } from "../utils/cleanTransformBlocks.js";
 
 export default async function Home() {
-  const { data } = await client.query({
-    query: gql`
-      query GetHomePage {
-        nodeByUri(uri: "/") {
-          ... on Page {
-            id
-            blocks(postTemplate: false)
-          }
-        }
-      }
-    `,
-  });
-
+  const data = await getHomePageData();
   const blocks = cleanTransformBlocks(data.nodeByUri.blocks);
-
   return (
     <div>
       {BlockRenderer({ blocks })}
