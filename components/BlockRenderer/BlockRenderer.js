14fabb136e372a45f5c71550357b1d6fbec0129e
rabindratharu <rabindra.tharu.np@gmail.com>
2026-05-05 08:04:44 +0545

    fixes paragraphh achor link color and hover color

--- components/BlockRenderer/BlockRenderer.js
+++ components/BlockRenderer/BlockRenderer.js
@@ -7,13 +7,14 @@ export const BlockRenderer = ({ blocks }) => {
         switch (block.name) {
             case 'core/paragraph':
                 return (
-                <Paragraph 
-                    key={block.id} 
-                    textAlign={block.attributes?.textAlign} 
-                    content={block.attributes?.content}
-                    textColor={block.attributes?.style?.color?.text || block.attributes?.textColor }
-                    />
-                );
+                    <Paragraph
+                        key={block.id}
+                        textAlign={block.attributes?.textAlign}
+                        content={block.attributes?.content}
+                        textColor={block.attributes?.style?.color?.text || block.attributes?.textColor}
+                        linkColor={block.attributes?.style?.elements?.link?.color?.text}
+                        linkHoverColor={block.attributes?.style?.elements?.link?.[":hover"]?.color?.text}
+                    />);
             case 'core/heading':
                 return <Heading key={block.id} textAlign={block.attributes?.textAlign} level={block.attributes?.level} content={block.attributes?.content} />;
             case 'core/cover':
--- components/Paragraph/Paragraph.js
+++ components/Paragraph/Paragraph.js
@@ -1,10 +1,30 @@
 import { getTextAlign, relativeToAbsoluteUrls } from "../../utils/index.js";
-export const Paragraph = ({ content, textAlign, textColor }) => {
+
+export const Paragraph = ({ content, textAlign, textColor, linkColor, linkHoverColor }) => {
+    // Only apply link styles if both linkColor and linkHoverColor are defined
+    const hasLinkStyles = linkColor !== undefined && linkHoverColor !== undefined;
+
     return (
         <p
             style={{ color: textColor }}
             dangerouslySetInnerHTML={{ __html: relativeToAbsoluteUrls(content) }}
-            className={`${getTextAlign(textAlign)} max-w-5xl mx-auto my-5`}
+            className={`
+                ${getTextAlign(textAlign)} 
+                max-w-5xl mx-auto my-5
+                ${hasLinkStyles ? `
+                    [&_a]:text-[var(--link-color)]
+                    [&_a:hover]:text-[var(--link-hover-color)]
+                    [&_a]:transition-colors
+                    [&_a]:duration-200
+                ` : ''}
+            `}
+            style={{
+                color: textColor,
+                ...(hasLinkStyles && {
+                    '--link-color': linkColor,
+                    '--link-hover-color': linkHoverColor,
+                })
+            }}
         />
     )
 }
